8cec22cc6978bc14612059c1d64b632ce5d9efa7
rakibi-alaa <rakibi.alae@gmail.com>
2020-05-28 12:56:59 +0100

    folders init (sreens, components) and installed styled-component via npm

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text>Open up App.js to start here  working on your app!</Text>
+    </SafeAreaView>
   );
 }
 
@@ -13,7 +13,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  
   },
 });
